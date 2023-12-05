8a79440b7c2434ec40d6d9fa307de0ebfb022f9e
garciaoyervides <garciaoyervides@gmail.com>
2023-12-05 01:58:41 +0100

    fix

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@ echo "Contextual Search App Installer will install a Python virtual environment"
 CWD=$(pwd)
 CUDA=$(nvidia-smi | grep "CUDA Version")
 python -m venv "$CWD"/venv || exit $1
+source $CWD/venv/bin/activate
 
 if [[ $CUDA == *"CUDA Version: 12"* ]]; then
     echo "CUDA 12.* detected"
-    $CWD/venv/bin/python pip3 install torch torchvision torchaudio
+    pip3 install torch torchvision torchaudio
 elif [[ $CUDA == *"CUDA Version: 11"* ]]; then
     echo "CUDA 11.* detected"
-    $CWD/venv/bin/python pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 else
     echo "No compatible CUDA detected"
     exit $1
